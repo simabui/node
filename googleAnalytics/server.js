b7b55469eaeea63361200624dbc0b618353dcd51
simabui <sima_sima@live.ru>
2021-05-18 22:53:46 +0300

    add getDailyData

--- googleAnalytics/server.js
+++ googleAnalytics/server.js
@@ -1,29 +1,70 @@
 const express = require("express");
 const { google } = require("googleapis");
+const { analyticsreporting } = require("googleapis/build/src/apis/analyticsreporting");
 require("dotenv").config();
 
 const app = express();
 
-const scopes = "https://www.googleapis/auth/analytics.readonly";
+const scopes = ["https://www.googleapis.com/auth/analytics", "https://www.googleapis.com/auth/analytics.edit"];
 const client_email = process.env.CLIENT_EMAIL;
 const private_key = process.env.PRIVATE_KEY.replace(/\\n/g, "\n");
 
 const jwt = new google.auth.JWT(client_email, null, private_key, scopes);
 
-// FIXME: fix credentials error
-async function getData() {
+async function getPropertiesList() {
   const response = await jwt.authorize();
 
   try {
-    constresult = await google.analytics("v3").management.webproperties.list({
+    const result = await google.analytics("v3").management.webproperties.list({
       auth: jwt,
       accountId: process.env.ACCOUNT_ID,
     });
-    console.log(result.data.totalResults);
+    return result.data.totalResults;
   } catch (e) {
     console.log(e);
   }
 }
+
+async function getDailyData(viewId, startDate, endDate, organic = false) {
+  const analyticsreporting = google.analyticsreporting({
+    version: "v4",
+    auth: jwt,
+  });
+
+  let filter = "";
+  if (organic) {
+    filter = "ga:medium==organic";
+  }
+
+  const res = await analyticsreporting.reports.batchGet({
+    requestBody: {
+      reportRequests: [
+        {
+          viewId: viewId,
+          dateRanges: [
+            {
+              startDate: startDate,
+              endDate: endDate,
+            },
+          ],
+          metrics: [
+            {
+              expression: "ga:sessions",
+            },
+          ],
+          filtersExpression: filter,
+        },
+      ],
+    },
+  });
+  return res.data.reposrts[0].data.totals[0].values[0];
+}
+
+// FIXME: get correct viewId
+async function getData() {
+  result = await getDailyData("272302484", "today", "today");
+}
+
 getData();
 
 const listener = app.listen(process.env.PORT, () => {
